bc5fb368c5a3896fa07c34a7b2df755446b94ea3
Carlos Arbizu <carlos2345532@gmail.com>
2022-06-24 21:24:58 -0500

    Finished AirBnB Clone

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@ import logo from './logo.svg';
 import Nav from './components/Nav';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import Person from './person.png';
+import MountainBike from './mountain-bike.png';
+import WeddingPhoto from './wedding-photography.png';
 function App() {
   return (
   <> 
   <Nav/>
   <Hero/>
-  <Card/>
+  <div className = 'cards'>
+  <Card
+    img = {Person}
+    rating = '5.0'
+    reviewCount = {6}
+    country = 'USA'
+    title = 'Life Lessons With Katie Zafarie'
+    price = {136}
+  />
+    <Card
+    img = {WeddingPhoto}
+    rating = '5.0'
+    reviewCount = {30}
+    country = 'USA'
+    title = 'Learn Wedding Photography'
+    price = {125}
+  />
+  <Card
+    img = {MountainBike}
+    rating = '4.8'
+    reviewCount = {2}
+    country = 'USA'
+    title = 'Group Mountain Biking'
+    price = {50}
+  />
+  </div>
   </>);
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,26 @@
 import React from "react";
-
-export default function Card() {
+import Person from '../person.png'
+const star = <svg width="1.25rem" height="1.25rem" viewBox="0 0 12 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M5.52447 0.463525C5.67415 0.00286925 6.32585 0.00286996 6.47553 0.463525L7.45934 3.49139C7.52628 3.6974 7.71826 3.83688 7.93487 3.83688H11.1186C11.6029 3.83688 11.8043 4.45669 11.4124 4.74139L8.83679 6.61271C8.66155 6.74003 8.58822 6.96572 8.65516 7.17173L9.63897 10.1996C9.78864 10.6602 9.2614 11.0433 8.86955 10.7586L6.29389 8.88729C6.11865 8.75997 5.88135 8.75997 5.70611 8.88729L3.13045 10.7586C2.73859 11.0433 2.21136 10.6602 2.36103 10.1996L3.34484 7.17173C3.41178 6.96572 3.33845 6.74003 3.16321 6.61271L0.587553 4.74139C0.195696 4.45669 0.397084 3.83688 0.881446 3.83688H4.06513C4.28174 3.83688 4.47372 3.6974 4.54066 3.49139L5.52447 0.463525Z" fill="#FE395C"/> 
+</svg> 
+export default function Card(props) {
     return (
         <div className = 'card'>
-            <img src='' alt='of Katie Zaferes'/>
+            <div className = 'imageHolder'>
+            <img width = '170px' height = '230px' src= {props.img} alt='of Katie Zaferes'/>
             <div className= 'onlineStatus'>SOLD OUT</div>
-            <div className = 'cardInfo'>
-                <div>5.0 <span className = 'locationText'>(6) <span className = 'ellipse'></span> USA </span></div>
-                <div className = 'description'>Life lessons with </div>
-                <div className= 'Pricing'><em>From $136</em> / person</div>
             </div>
+            <div className = 'cardInfo'>
+                <p className = 'cardInfoFirstLine'> {star} 
+                {props.rating} <span className = 'locationText'> ({props.reviewCount}) <span className = 'dot'>• </span> {props.country} </span>
+                </p>
+                <p>
+                {props.title}
+                </p>
+                <p>
+                <strong> From ${props.price} </strong> / person
+                </p>
+       </div>
         </div>
     )
 }
